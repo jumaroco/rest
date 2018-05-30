24d58ae51a0b15af1b3f516eba34f3bf42ddd84f
jumaroco <jmrc25@hotmail.com>
2018-05-30 10:47:30 -0600

    30052018_1047

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -26,7 +26,45 @@ class Login extends REST_Controller {
             'mensaje' => 'La información enviada no es válida'
      );
         $this->response( $respuesta, REST_Controller::HTTP_BAD_REQUEST);
+        return;
      }
+
+     $condiciones = array('usuario' => $data['usuario'],
+                          'password' => $data['password'] );
+
+     $query = $this->db->get_where('usuario', $condiciones);
+     $usuario = $query->row();
+
+     if( !isset( $usuario)){
+        $respuesta = array(
+            'error' => TRUE,
+            'mensaje' => 'Usuario y/o password invalido.'
+        );
+        $this->response( $respuesta);
+        return;
+     }
+
+     // usuario valido
+     //TOKEN
+     $token = bin2hex( openssl_random_pseudo_bytes(20));
+     //$token = hash( 'ripemd160', $data['usuario'] );
+     $token = hash( 'ripemd160', $usuario->email );
+
+
+     //Guardar en db el token
+     $this->db->reset_query();
+     $actualizar_token = array('token'=> $token);
+     $this->db->where( 'id', $usuario->id );
+
+     $hecho = $this->db->update( 'usuario', $actualizar_token);
+
+     $respuesta = array(
+        'error' => FALSE,
+        'token' => $token,
+        'id_usuario' => $usuario->id
+    );
+
+     $this->response($respuesta);
 
    }
 
